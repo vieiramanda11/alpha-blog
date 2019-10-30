f9981d6c3aee8a88e5a53e422f3f27f7bdafdc19
vieiramanda11 <amanda_vieira@outlook.com>
2019-10-30 18:32:19 -0300

    add user data to articles listing page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :user  
   validates :title, :description, presence: true
   validates :title, length: { in: 5..25 }
   validates :description, length: { minimum: 5 }
