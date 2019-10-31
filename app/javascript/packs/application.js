9b8d14d50e71f6ec26a227fd0fe638db29d9fc33
vieiramanda11 <amanda_vieira@outlook.com>
2019-10-31 19:56:03 -0300

    modifying files to fix boostrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,15 +9,13 @@
 //= require rails-ujs
 //= require_tree .
 
-import 'bootstrap';
+import 'bootstrap'
 
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("bootstrap/dist/js/bootstrap")
-require("packs/src/application.scss")
-require("../assets/stylesheets/custom.scss")
+require("packs/src/example.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
