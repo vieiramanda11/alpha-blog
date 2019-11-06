c4df3f3cf6d0ede3553896783f001bf1b3605a15
vieiramanda11 <amanda_vieira@outlook.com>
2019-11-06 19:21:24 -0300

    creating test to invalid category submission

--- spec/features/create_categories_spec.rb
+++ spec/features/create_categories_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe 'Creating a category', type: :feature do
     visit categories_path
     expect(page).to have_content("Health")
   end
+
+  scenario "invalid category submission results in failure" do
+    visit new_category_path
+    expect(page).to have_current_path(new_category_path)
+    fill_in "Name", with: " "
+    click_on "Create Category"
+    visit new_category_path
+    expect(page).to have_field("Name")
+  end
 end
