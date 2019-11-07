fa1961a759cde3d8bdc3b80358cbb0b4a9c01e99
vieiramanda11 <amanda_vieira@outlook.com>
2019-11-07 19:53:11 -0300

    creating article category model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ApplicationRecord
-  belongs_to :user  
+  belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   validates :title, :description, presence: true
   validates :title, length: { in: 5..25 }
   validates :description, length: { minimum: 5 }
 end
+
