372d311167975252bff6598e62a32fa9adf604cb
vieiramanda11 <amanda_vieira@outlook.com>
2019-11-07 17:36:52 -0300

    adding tests to guarantee only a admin can create categories

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe CategoriesController, type: :controller do
+
+  subject { User.new(username: "Amanda", email: "amanda@gmail.com", password: "amanda", admin: true) }
+  before { subject.save }
+
   describe "GET #index" do
     it "should return a 200 OK status" do
       get :index 
@@ -10,6 +14,7 @@ RSpec.describe CategoriesController, type: :controller do
 
   describe "GET #new" do
     it "should return a 200 OK status" do
+      session[:user_id] = subject.id
       get :new
       expect(response).to have_http_status(:ok)
     end
@@ -19,6 +24,15 @@ RSpec.describe CategoriesController, type: :controller do
     it "should return a 200 OK status" do
       get :show, params: { id: "1" }
       expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "#create" do
+    it 'should redirect create when admin not logged in' do
+      expect do
+        post :create, params: { category: { name: "Sports" } }
+      end.to_not change { Category.count }
+      expect(response).to redirect_to(categories_path)
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,9 +7,11 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'spec_helper'
 require 'capybara/rspec'
+# require 'devise'
+# require 'support/controller_macros'
 # Add additional requires below this line. Rails is not loaded until this point!
-
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -36,7 +38,9 @@ end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
+  # config.include Devise::Test::ControllerHelpers, :type => :controller
+  # config.include Warden::Test::Helpers
+  # config.extend ControllerMacros, :type => :controller
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
