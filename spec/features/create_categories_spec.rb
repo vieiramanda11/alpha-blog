75b2f14b3ff863b960b0bb3c3440759dbe9bf2de
vieiramanda11 <amanda_vieira@outlook.com>
2019-11-07 19:10:31 -0300

    adding integration test to verify that only admin visit and create new categories

--- spec/features/create_categories_spec.rb
+++ spec/features/create_categories_spec.rb
@@ -1,21 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a category', type: :feature do
+  subject { User.new(username: "Amanda", email: "amanda@gmail.com", password: "amanda", admin: true) }
+  before { subject.save }
+
   scenario "get new category form and create category" do
+    sign_in_as(subject, "amanda")
     visit new_category_path
-    expect(page).to have_current_path(new_category_path)
-    fill_in "Name", with: "Health"
-    click_on "Create Category"
-    visit categories_path
-    expect(page).to have_content("Health")
+    expect do
+      post categories_path, params: { category: { name: "Health" } }
+    end.to change { Category.count }
   end
 
   scenario "invalid category submission results in failure" do
+    sign_in_as(subject, "amanda")
     visit new_category_path
-    expect(page).to have_current_path(new_category_path)
-    fill_in "Name", with: " "
-    click_on "Create Category"
-    visit new_category_path
-    expect(page).to have_field("Name")
+    expect do
+      post categories_path, params: { category: { name: " " } }
+    end.to_not change { Category.count }
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -44,8 +44,9 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
+  config.include RSpec::Rails::RequestExampleGroup, type: :feature
   config.use_transactional_fixtures = true
-
+  # config.infer_spec_type_from_file_location!
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
@@ -53,16 +54,22 @@ RSpec.configure do |config|
   # You can disable this behaviour by removing the line below, and instead
   # explicitly tag your specs with their type, e.g.:
   #
-  #     RSpec.describe UsersController, :type => :controller do
-  #       # ...
-  #     end
+      # RSpec.describe UsersController, :type => :controller do
+      #   # ...
+      # end
   #
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
   # Filter lines from Rails gems in backtraces.
-  config.filter_rails_from_backtrace!
+  config.filter_rails_from_backtrace!      
+  # RSpec.describe UsersController, :type => :controller do
+  #   # ...
+  # end
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  def sign_in_as(user, password)
+    post login_path, params: { session: {email: user.email, password: password} } 
+  end
 end
